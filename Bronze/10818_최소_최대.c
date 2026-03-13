11c3eaebe58b4711fa393e86b99ef3afa1c372f3
scalarnvector <wings2248@gmail.com>
2026-03-13 23:44:18 +0900

    Update 10818_최소_최대.c

--- Bronze/10818_최소_최대.c
+++ Bronze/10818_최소_최대.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 int main() {
-	int num;
+	int num, temp;
+	int 
 	scanf("%d", &num);
+	
+	for (int i = 0; i < num; i++)
+	scanf("%d", &temp);
+	
+	for (int i = 0; i < )
 	return 0;
 }
