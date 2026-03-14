5beccd4ee6eacddd7f556c4c816f5700fbee5fee
scalarnvector <wings2248@gmail.com>
2026-03-14 13:28:24 +0900

    baekjoon update

--- Bronze/10818_최소_최대.c
+++ Bronze/10818_최소_최대.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 int main() {
 	int num, temp;
-	int 
+	int best = -1000001;
+	int min = 1000001;
 	scanf("%d", &num);
 	
-	for (int i = 0; i < num; i++)
-	scanf("%d", &temp);
-	
-	for (int i = 0; i < )
+	for (int i = 0; i < num; i++) {
+		scanf("%d", &temp);
+		if (best < temp)
+			best = temp;
+		if (min > temp)
+			min = temp;
+	}
+	printf("%d %d\n", min, best);
 	return 0;
 }
